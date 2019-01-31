056cb1db4f3d06e79779d750a3f9174df8df92ca
Unknown <leptest@gmail.com>
2019-01-31 20:20:07 +0000

    changes

--- components/CardsWrapper.js
+++ components/CardsWrapper.js
@@ -1,66 +1,61 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import Player from "./Player";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Player from './Player';
 
 class CardsWrapper extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      player1: "",
-      player2: ""
-    };
-    this.pickTwoRandom = this.pickTwoRandom.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state={
+			player1: '',
+			player2: ''
+		}
+		this.pickTwoRandom = this.pickTwoRandom.bind(this);
 
-  componentDidMount() {
-    this.pickTwoRandom();
-  }
+	}
 
-  pickTwoRandom() {
-    const allPayers = this.props.players;
-
-    const index1 = Math.floor(Math.random() * allPayers.length);
-    const index2 = Math.floor(Math.random() * allPayers.length);
-
-    if (index1 === index2) {
-      pickTwoRandom();
-    }
-
-    const player1 = allPayers[index1];
-    const player2 = allPayers[index2];
-  }
-  // onChoice() {
-  // }
-  //
-  // checkScore() {
-  // }
-  //
-  // updateScore() {
-  // }
-
-  render() {
-    const allPlayers = this.props.allPlayers;
-
-    const test = {
-      name: "daniel chapman",
-      image: "https://d17odppiik753x.cloudfront.net/playerimages/nba/15860.png",
-      fppg: "15.124312"
-    };
-    return (
-      <View style={styles.container}>
-        <Player player={test} />
-        <Player player={test} />
-      </View>
-    );
-  }
+componentDidMount(){
+	this.pickTwoRandom();
 }
 
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between"
-  }
-});
+	pickTwoRandom() {
+		const allPayers = this.props.players;
+
+		const index1 = Math.floor(Math.random() * allPayers.length);
+		const index2 = Math.floor(Math.random() * allPayers.length);
+
+		console.log(index1);
+		console.log(index2);
+
+		if(index1 === index2){
+			pickTwoRandom();
+		}
+
+		this.setState({
+			player1: allPayers[index1],
+			player2: allPayers[index2]
+		}, () => { console.log(this.state)});
+
+	}
+	// onChoice() {
+	// }
+	//
+	// checkScore() {
+	// }
+	//
+	// updateScore() {
+	// }
+
+	render() {
+		const allPlayers = this.props.allPlayers;
+
+		return (
+			<View>
+          <Player player={this.state.player1} />
+		      <Player player={this.state.player2} />
+			</View>
+		);
+	}
+
+}
 
 export default CardsWrapper;
